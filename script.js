aa9dd2faa075632c1f54ead4201ed327644112d1
Haze-UK <166338921+Haze-UK@users.noreply.github.com>
2025-06-07 22:41:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,33 @@
 const testimonials = [
   {
     stars: 5,
-    text: "Excellent team. Very responsive and professional. I would recommend them to anyone!",
-    author: "Jane D.",
-    meta: "2 months ago · Google"
+    text: "Eddie worked with great skill and professionalism. He built our flat roof quickly and cleanly. A job well done",
+    author: "Vihaan Teo.",
+    meta: "6 days ago · Google"
   },
   {
-    stars: 4,
-    text: "Quick repair and transparent pricing. I appreciated the photo updates during the work.",
-    author: "John R.",
-    meta: "1 month ago · Google"
+    stars: 5,
+    text: "Eddie completed the chimney repair without a hitch. His work was tidy, quick, and professional. We couldn’t have asked for better service.",
+    author: "Nicholas Baker.",
+    meta: "2 weeks ago · Google"
+  },
+    {
+    stars: 5,
+    text: "Peter was excellent in every aspect of the build. He handled the project smoothly and professionally. The flat roof turned out great. Highly skilled and trustworthy.",
+    author: "Zahir Boden.",
+    meta: "3 week ago · Google"
+  }
+      {
+    stars: 5,
+    text: "Eddie did an amazing job on our flat roof. He worked with a high level of precision and care. His professionalism stood out. The result exceeded expectations.",
+    author: "Orlando Yael.",
+    meta: "1 week ago · Google"
+  }
+      {
+    stars: 5,
+    text: "James handled our new flat roof project like a pro. He stayed organized and delivered great results. Very efficient and easy to work with. Superb job.",
+    author: "aRudy Eliel.",
+    meta: "1 week ago · Google"
   }
 ];
 
@@ -51,4 +69,3 @@ document.getElementById('settingsToggle').addEventListener('click', () => {
   document.getElementById('settingsPanel').classList.toggle('active');
   document.getElementById('mobileMenu').classList.remove('active'); // Close menu if open
 });
-
